07a0518cf3e7c9bdf062141257d3f5623d3f2372
Jonathan-MG <jonathanmgomes10@hotmail.com>
2023-02-25 08:44:17 -0300

    Introdução ao JavaScript

---
+++ Aula 01/script.js
@@ -0,0 +1,37 @@
+// const, let, var - Para declarar variáveis
+
+// const nome1 = "Ana"
+// const nome2 = 'Pedro'
+// const nome3 = `José`
+// console.log(typeof(nome1))
+// console.log(typeof(nome2))
+// console.log(typeof(nome3))
+// nome1 = 'João'
+// REPL: Read, Evaluate, Print, Loop.
+// const não pode assumir outro valor.
+
+//ECMA Script
+// let a = 2
+// let b = 'abc'
+// console.log(typeof(b))
+// b = 12
+// console.log(typeof(b))
+// b = true
+// console.log(typeof(b))
+// let permite que a variáel assuma qualquer valor.
+
+// var endereco = 'Rua C'
+// console.log(endereco)
+// endereco = 'Rua B'
+// console.log(endereco)
+// console.log(typeof(endereco))
+// var linguagem = 'Javascript'
+// console.log("Aprendendo " + linguagem)
+// var linguagem = 'Java'
+// console.log("Aprendendo " + linguagem)
+// var idade = 18
+// console.log('Oi, ' + nome)
+// if (idade >= 18){
+//     var nome = 'João'
+// }
+// console.log('Oi, ' + nome)
