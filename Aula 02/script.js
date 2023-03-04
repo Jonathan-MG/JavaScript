60ffc61beed310e8f121373a43fce84a78ad9266
JONATHAN MARTINS GOMES <20.00862-7@maua.br>
2023-03-04 09:12:06 -0300

    Commit de fim de aula

--- Aula 02/script.js
+++ Aula 02/script.js
@@ -36,3 +36,44 @@
 // const soma = valores.reduce((ac, v) => {return ac + v})
 // console.log(soma)
 
+// Funções
+// function hello(){
+//     console.log('Hello!')
+// }
+// hello()
+
+// function hello(nome){
+//     console.log('Hello ' + nome)
+// }
+// hello('Jonathan')
+// function soma(a, b){
+//     return a + b
+// }
+// console.log(soma(2,3))
+
+// const dobro =  function (n){
+//     return 2 * n
+// }
+// console.log(dobro(5));
+
+// const triplo = function(n = 5){
+//     return n * 3
+// }
+// console.log(triplo())
+// console.log(triplo(7))
+
+// Dobro = n => {return 2*n} // Se for só 1 parâmetro não precisa de ()
+// console.log(Dobro(2))
+
+// Soma = (a,b) => a + b 
+// console.log(Soma(3, 7))
+
+// const teste = (a, b, c) => a*b*c //Expressão com uma única linha não precisa de return
+// console.log(teste(1, 2, 3))
+
+// const hello = () => console.log('Hello!')
+// hello()
+
+// const dobro = valor => valor * 2
+// console.log(dobro(2))
+
