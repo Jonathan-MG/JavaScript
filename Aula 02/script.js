f61397772bb3182b3834a02fae18b547cb66eb68
JONATHAN MARTINS GOMES <20.00862-7@maua.br>
2023-03-04 08:36:40 -0300

    Primeiro commit da aula

---
+++ Aula 02/script.js
@@ -0,0 +1,38 @@
+// Aula 02 - Vetores
+
+// v1 = []
+// v1[0] = 3.4
+// v1[10] = 2
+// console.log(v1)
+// for (let i = 0; i < v1.length; i++)
+//     console.log(v1[i])
+
+// v = [2, 'abc', true]
+
+// function soma(a, b){
+//     return a + b
+// }
+// console.log(soma(2,3))
+
+// const nomes = ['Ana Maria', 'Antonio', 'Rodrigo', 'Alex', 'Cristina']
+      
+// Filter e arrow function
+// const teste = () => {console.log('Minha arrow function')}
+// teste()
+// const resultado = nomes.filter((nome) => {return nome.startsWith('A')})
+// console.log(resultado)
+
+// Mapeamento
+//['Ana', 'Pedro'] => ['A', 'P']
+//const letrasIniciais = nomes.map((nome) => {return nome.charAt(0)})
+//console.log(letrasIniciais)
+
+// const numeros = [1, 2, 3, 4, 5]
+// [1, 2, 3, 4, 5] => [1, 4, 9, 16, 25]
+// const Quadrado = numeros.map((numero) => {return numero**2})
+// console.log(Quadrado)
+
+// const valores = [1, 2, 3, 4]
+// const soma = valores.reduce((ac, v) => {return ac + v})
+// console.log(soma)
+
