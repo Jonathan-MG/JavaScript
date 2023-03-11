c0378b63a7e2ad1458b1fbd64fb4b37be3a1a795
JONATHAN MARTINS GOMES <20.00862-7@maua.br>
2023-03-11 09:14:50 -0300

    Fim da aula.

--- Aula 03/script.js
+++ Aula 03/script.js
@@ -75,3 +75,68 @@
 // let eAgoraResult = eAgora()
 // eAgoraResult.f1()
 // eAgoraResult.f2()
+
+// -------------------------FIM DO CAPÍTULO 1-------------------------------
+// -------------------------------------------------------------------------
+// Uma pessoa que se chama João e tem 17 anos.
+// let pessoa = {
+//     nome: 'João',
+//     idade: 17
+// }
+
+// Uma pessoa que se chama Maria, tem 21 anos, e mora na rua B, número 121.
+// let pessoa = {
+//     nome: 'Maria',
+//     idade: 21,
+//     endereco: {
+//         rua: 'Rua B',
+//         número: 121
+//     }
+// }
+
+// Uma concessionária tem CNPJ e endereço (Logradouro e Número). Ela possui três veículos no seu estoque. Cada veículo tem modelo,
+// marca e ano de fabricação.
+
+// let concessionaria = {
+//     nome: 'MotorSport',
+//     cnpj: '61.531.661/0001-02',
+//     endereco: {
+//         logradouro: 'Rua das carroças',
+//         numero: 121
+//     },
+//     veiculos: [{
+//             modelo: 'Vectra',
+//             marca: 'GM',
+//             ano: 2006
+//         },{
+//             modelo: 'Polo',
+//             marca: 'Volkswagen',
+//             ano: 2010
+//         },{
+//             modelo: 'Fiesta',
+//             marca: 'Ford',
+//             ano: 2015
+//         }]
+// }
+
+//. e [''] - As duas formas de acesso.
+// console.log(pessoa.nome);
+// console.log(pessoa['idade']);
+
+// console.log(pessoa.endereco);
+// console.log(pessoa['endereco'].rua);
+// console.log(pessoa.endereco['número']);
+// console.log(pessoa.endereco.rua);
+
+// console.log(concessionaria);
+// for (let veiculo of concessionaria.veiculos){
+//     console.log('Modelo: ' + veiculo.modelo + ', Marca: ' + veiculo.marca + ', Ano: ' + veiculo.ano);
+// }
+
+// Calculadora que soma dois valores e subtrai um valor de outro.
+// let calc = {
+//     soma: (a,b) => a + b,
+//     subtracao: (a,b) => a - b,
+// }
+// console.log(calc.soma(2,3));
+// console.log(calc['subtracao'](5,3));
