71db1b80cfa41c8c82501ea7a41e4facb5079de9
JONATHAN MARTINS GOMES <20.00862-7@maua.br>
2023-06-03 08:52:35 -0300

    Exercicios 1 ao 4!

---
+++ Aula 11/Db/Db_medicos.sql
@@ -0,0 +1,27 @@
+CREATE TABLE tb_medico(
+	cod_medico SERIAL PRIMARY KEY,
+	crm VARCHAR (20) UNIQUE NOT NULL,
+	nome VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE tb_paciente(
+	cod_paciente SERIAL PRIMARY KEY,
+	cpf VARCHAR (20) UNIQUE NOT NULL,
+	nome VARCHAR(20) NOT NULL,
+	idade INT NULL
+);
+
+CREATE TABLE tb_consulta(
+	cod_consulta SERIAL PRIMARY KEY,
+	cod_medico INT NOT NULL,
+	cod_paciente INT NOT NULL,
+	data_hora TIMESTAMP NOT NULL,
+	CONSTRAINT fk_medico FOREIGN KEY (cod_medico) REFERENCES tb_medico(cod_medico),
+	CONSTRAINT fk_paciente FOREIGN KEY (cod_paciente) REFERENCES tb_paciente(cod_paciente)
+);
+
+INSERT INTO tb_medico (crm, nome) VALUES ('123456', 'Ana'), ('987654', 'Pedro');
+SELECT * FROM tb_medico;
+
+INSERT INTO tb_paciente (cpf, nome, idade) VALUES ('12345678910', 'Carlos Alberto', '21'), ('10312342101', 'Paula Arósio', '45');
+SELECT * FROM tb_paciente;
---
+++ Aula 11/index.js
@@ -0,0 +1,85 @@
+require('dotenv').config()
+const {
+    PG_HOST,
+    PG_USER,
+    PG_PASSWORD,
+    PG_DATABASE,
+    PG_PORT,
+    PORT
+} = process.env
+const { Client } = require('pg')
+const express = require('express')
+const { ssl } = require('pg/lib/defaults')
+const app = express()
+app.use(express.json())
+
+app.get('/medicos', (req,res) => {
+    const client = new Client({
+        host: PG_HOST,
+        user: PG_USER,
+        password: PG_PASSWORD,
+        database: PG_DATABASE,
+        port: PG_PORT
+    })
+    client.connect()
+    client.query('SELECT * FROM tb_medico')
+    .then(db_Result => {
+        res.status(200).json(db_Result.rows)
+        client.end()
+    })
+    .catch(e => {
+        console.log(e)
+        res.status(500).end()
+        client.end()
+    })
+})
+
+app.get('/pacientes', (req,res) => {
+    const client = new Client({
+        host: PG_HOST,
+        user: PG_USER,
+        password: PG_PASSWORD,
+        database: PG_DATABASE,
+        port: PG_PORT
+    })
+    client.connect()
+    client.query('SELECT * FROM tb_paciente')
+    .then(db_Result => {
+        res.status(200).json(db_Result.rows)
+        client.end()
+    })
+    .catch(e => {
+        console.log(e)
+        res.status(500).end()
+        client.end()
+    })
+})
+
+app.post('/adicionar-medico', (req,res) => {
+    const client = new Client({
+        host: PG_HOST,
+        user: PG_USER,
+        password: PG_PASSWORD,
+        database: PG_DATABASE,
+        port: PG_PORT
+    })
+    const crm = req.body.crm
+    const nome = req.body.nome
+    client.connect()
+    client.query('INSERT INTO tb_medico (crm, nome) VALUES ($1, $2)',[crm,nome])
+    .then(() => {
+        res.send(200).json("Ok!")
+        client.end()
+    })
+    .catch(e => {
+        console.log(2)
+        res.send(500).end()
+        client.end()
+    })
+
+})
+
+app.listen(
+    PORT,
+    () => console.log(`Servidor OK. Porta: ${PORT}`)
+)
