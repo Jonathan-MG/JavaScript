64a840de980b5c8cd118b553e9cdda7ab6cc6bc5
JONATHAN MARTINS GOMES <20.00862-7@maua.br>
2023-06-03 09:25:23 -0300

    Troca do Client pelo Pool!

--- Aula 11/index.js
+++ Aula 11/index.js
@@ -7,74 +7,57 @@ const {
     PG_PORT,
     PORT
 } = process.env
-const { Client } = require('pg')
+const { Pool } = require('pg')
 const express = require('express')
 const { ssl } = require('pg/lib/defaults')
 const app = express()
 app.use(express.json())
 
+const pool = new Pool({
+    host: PG_HOST,
+    user: PG_USER,
+    password: PG_PASSWORD,
+    database: PG_DATABASE,
+    port: PG_PORT
+})
+
+pool.on('error', (err,client) => {
+    console.error('Erro inexperado com o client', err)
+    process.exit(-1)
+})
+
 app.get('/medicos', (req,res) => {
-    const client = new Client({
-        host: PG_HOST,
-        user: PG_USER,
-        password: PG_PASSWORD,
-        database: PG_DATABASE,
-        port: PG_PORT
-    })
-    client.connect()
-    client.query('SELECT * FROM tb_medico')
+    pool.query('SELECT * FROM tb_medico')
     .then(db_Result => {
         res.status(200).json(db_Result.rows)
-        client.end()
     })
     .catch(e => {
         console.log(e)
         res.status(500).end()
-        client.end()
     })
 })
 
 app.get('/pacientes', (req,res) => {
-    const client = new Client({
-        host: PG_HOST,
-        user: PG_USER,
-        password: PG_PASSWORD,
-        database: PG_DATABASE,
-        port: PG_PORT
-    })
-    client.connect()
-    client.query('SELECT * FROM tb_paciente')
+    pool.query('SELECT * FROM tb_paciente')
     .then(db_Result => {
         res.status(200).json(db_Result.rows)
-        client.end()
     })
     .catch(e => {
         console.log(e)
         res.status(500).end()
-        client.end()
     })
 })
 
 app.post('/adicionar-medico', (req,res) => {
-    const client = new Client({
-        host: PG_HOST,
-        user: PG_USER,
-        password: PG_PASSWORD,
-        database: PG_DATABASE,
-        port: PG_PORT
-    })
     const crm = req.body.crm
     const nome = req.body.nome
-    client.connect()
-    client.query('INSERT INTO tb_medico (crm, nome) VALUES ($1, $2)',[crm,nome])
+    pool.query('INSERT INTO tb_medico (crm, nome) VALUES ($1, $2)',[crm,nome])
     .then(() => {
-        res.send(200).json("Ok!")
-        client.end()
+        res.status(200).json("Ok!")
     })
     .catch(e => {
-        console.log(2)
-        res.send(500).end()
-        client.end()
+        console.log(e)
+        res.status(500).end()
     })
 
 })
